ca34db23586dd401f4011139d2e46d47159e2715
Mark Howard <mark.howard@terminalfour.com>
2017-04-13 09:32:38 +0100

    Adding PHP functionality for apache2

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 apt-get update
 apt-get --yes --force-yes install apache2
+apt-get --yes install php5 libapache2-mod-php5
 cp -f /shared/000-default.conf /etc/apache2/sites-enabled
 service apache2 restart
